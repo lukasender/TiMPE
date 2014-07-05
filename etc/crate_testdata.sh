3bff9191539cd4fd34fc81919e2cf158bb18f692
Lukas Ender <hello@lukasender.at>
2014-07-05 12:16:37 +0200

    test data: increase initial balance

--- etc/crate_testdata.sh
+++ etc/crate_testdata.sh
@@ -1,8 +1,8 @@
-curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"elon_musk","balance":100000.00}'
+curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"elon_musk","balance":10000000.00}'
 echo ""
-curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"albert_einstein","balance":100000.00}'
+curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"albert_einstein","balance":10000000.00}'
 echo ""
-curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"nikola_tesla","balance":100000.00}'
+curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"nikola_tesla","balance":10000000.00}'
 echo ""
-curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"lovelace","balance":100000.00}'
+curl -XPOST -H "Content-Type: application/json" http://localhost:9100/users/register -d '{"nickname":"lovelace","balance":10000000.00}'
 echo ""
